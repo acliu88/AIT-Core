09666e832469d1a7547fa5dd1432fd80f756f2cf
Alyssa Liu <41084581+acliu88@users.noreply.github.com>
2020-08-07 14:09:59 -0700

    Updated with newest file edits
    Added argparse testing and sender function

--- ait/core/tlmChnel/tlmServer.py
+++ ait/core/tlmChnel/tlmServer.py
@@ -13,14 +13,60 @@ class UDPServer(object):
                                                       self.udp_server.started)
 
     def handler(self, data, address):
-        print "{}: got data {} from {}".format(self, data, address)
+        print ("{}: got data {} from {}".format(self, data, address))
 
     def stop(self):
         self.udp_server.close()
 	
+def sender(host, timeout=1):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    print ("start sending data to", host)
+    with Timeout(timeout, exception=False):
+        while(True):
+            rand = random.random()
+            print ("send", rand)
+            sock.sendto(str(rand), host)
+            sleep(.1)
+    print "sender finished"
+
+def plain_datagram_server(host, port, time):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+    # Bind the socket to the port
+    print ("start plain UDP server for {}s".format(time))
+    server_address = (host, port)
+    print ('starting up on %s port %s' % server_address)
+    sock.bind(server_address)
+    print ("started plain datagram server")
+    sleep(time)
+    print ("stop plain datagram server")
+
+
+def test_datagram_server(host):
+    print ("DatagramServer test on", host)
+    print ("Start udp1 on {} and send data".format(host))
+    udp1 = UDPServer(host)
+    sender_greenlet = spawn(sender, host)
+    sender_greenlet.join()
+
+
 if __name__ == '__main__':
-	log.info('Starting Telemetry Channel Server')
-	UPDServer(':9000').serve_forever()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("mode", choices=["send", "test_plain"])
+    parser.add_argument("--time", default=10)
+    parser.add_argument("--port", default=8000)
+    parser.add_argument("--host", default="localhost")
+
+    args = parser.parse_args()
+    if args.mode == "send":
+        sender((args.host, args.port), args.time)
+    elif args.mode == "test_plain":
+        spawn(plain_datagram_server, args.host, args.port, args.time)
+        sleep()
+        test_datagram_server((args.host, args.port))
+
 
 
 
