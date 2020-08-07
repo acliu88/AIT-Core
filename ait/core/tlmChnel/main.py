57a705162593208928af8ceed8a959aeced0df10
Alyssa Liu <41084581+acliu88@users.noreply.github.com>
2020-08-07 11:40:09 -0700

    Temporarily remove argparse

--- ait/core/tlmChnel/main.py
+++ ait/core/tlmChnel/main.py
@@ -9,15 +9,15 @@ Usage: ait-channelTlm
 Starts up server to read in telemetry and 
 build AIT Packets onces per second
 
-'''
-
-def main():
-	
 	ap = argparse.ArgumentParser(
 		description = __doc__,
 		formatter_class = argparse.ArgumentDefaultsHelpFormatter
 		)
 	args = ap.parse_args()
+'''
+
+def main():
+	
 	log.info('Starting Telemetry Channel Server')
 	tlmChannel(':3076').serve_forever()
 
