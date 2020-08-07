4141dd777d357773dfc1ce7ff9fff9a0611ce113
Alyssa Liu <41084581+acliu88@users.noreply.github.com>
2020-08-07 13:50:27 -0700

    Update tlmServer.py

--- ait/core/tlmChnel/tlmServer.py
+++ ait/core/tlmChnel/tlmServer.py
@@ -2,11 +2,22 @@ from __future__ import print_function
 from gevent.server import DatagramServer
 from ait.core import log
 
-class tlmChannel(DatagramServer):
+class UDPServer(object):
 
-	def handle(self, data, address): 
-		print(data)
+    def __init__(self, host):
+        self.udp_server = DatagramServer(host, handle=self.handler)
+        self.udp_server.start()
 
+    def __repr__(self):
+        return "<UDPServer id={} started: {}>".format(id(self),
+                                                      self.udp_server.started)
+
+    def handler(self, data, address):
+        print "{}: got data {} from {}".format(self, data, address)
+
+    def stop(self):
+        self.udp_server.close()
+	
 if __name__ == '__main__':
 	log.info('Starting Telemetry Channel Server')
 	tlmChannel(':9000').serve_forever()
